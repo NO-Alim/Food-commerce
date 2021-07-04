63abfc40931dd318be9baea61b99abdcee1e8bd9
Abdul Alim <abdulalimrakib53@gmail.com>
2021-07-05 03:43:42 +0600

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,142 @@
+import React,{useState,useEffect} from 'react'
+import { useGlobalContext } from '../context';
+import {NavLink} from 'react-router-dom'
+import {FaSun,FaMoon,FaTimes} from 'react-icons/fa'
+import {FiMenu,FiShoppingCart,FiSearch} from 'react-icons/fi'
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
+import './scss/Navbar.scss'
+
+
+const img = 'https://cdn.shopify.com/s/files/1/0162/3932/9344/files/logo-white_160x.png?v=1547879955'
+
+const useStyles = makeStyles({
+    root: {
+        display: 'flex',
+      },
+    list: {
+        width: 250,
+      },
+      fullList: {
+        width: 'auto',
+      },
+      drawerPaper : {
+          width: 250,
+          background: "#0b162a",
+          color: "#e9e9e9"
+      }
+  });
+
+const getStorageTheme = () => {
+    let theme = 'light-theme'
+    if (localStorage.getItem('theme')) {
+      theme = localStorage.getItem('theme');
+    }
+    return theme;
+  }
+
+  
+
+const Navbar = () => {
+    const classes = useStyles();
+    const {product} = useGlobalContext();
+  const [theme, setTheme] = useState(getStorageTheme());
+  //drawer
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme')
+    } else{
+      setTheme('light-theme')
+    }
+  }
+
+  const toggleDrawer = () =>{
+    setDrawerOpen(!drawerOpen);
+}
+
+const toggleCartDrawer = () => {
+    setCartDrawerOpen(!cartDrawerOpen);
+}
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme',theme);
+  }, [theme])
+
+    return (
+        <div>
+            <nav>
+                <div className="nav-container">
+                    <div className="link-container">
+                        <ul>
+                            <li><NavLink to="#">Home</NavLink></li>
+                            <li><NavLink to="#">shop</NavLink></li>
+                            <li><NavLink to="#">product</NavLink></li>
+                            <li><NavLink to="#">contact</NavLink></li>
+                        </ul>
+                    </div>
+                    <div className="logo-container">
+                        <div className="logo">
+                            <h1>ncare</h1>
+                        </div>
+                    </div>
+                    <div className="cart-menu">
+                        <div className="search-container">
+                            <div className="search-btn">
+                                <span><FiSearch/></span>
+                            </div>
+                            <form>
+                                <input type="text" placeholder="search"/>
+                                <input type="submit" />
+                            </form>
+                        </div>
+                        <div className="cart-container">
+                            <div className="dark-mood">
+                            <span onClick={() => toggleTheme()}>{theme === "light-theme" ? <FaMoon />: <FaSun />}</span>
+                            </div>
+                            <div className="cart-icon">
+                                <span onClick={() => toggleCartDrawer()}><FiShoppingCart/></span>
+                                <span className="item-total">0</span>
+                            </div>
+                            <div className="cart-items-price">
+                                <span>0<span className="spacial-tag">/</span>$0.00</span>
+                            </div>
+                            <div className="menu">
+                                <span onClick={() => toggleDrawer()}><FiMenu /></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <Drawer className={classes.list}
+            palette="secondary"
+            variant="persistent"
+            open={drawerOpen}
+            anchor="left"
+            classes={{paper: classes.drawerPaper}}>
+                <div className="drawer-container menu-drawer">
+                    <div className="drawer-btn-container">
+                        <button onClick={() => setDrawerOpen(false)} className="close-btn"><FaTimes /></button>
+                    </div>
+                </div>
+        </Drawer>
+        <Drawer className={classes.list}
+            palette="secondary"
+            variant="persistent"
+            open={cartDrawerOpen}
+            anchor="right"
+            classes={{paper: classes.drawerPaper}}>
+                <div className="drawer-container cart-drawer">
+                    <div className="drawer-btn-container">
+                        <button onClick={() => setCartDrawerOpen(false)} className="close-btn"><FaTimes /></button>
+                    </div>
+                </div>
+        </Drawer>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/context.js
@@ -0,0 +1,66 @@
+import React,{useStatek,useContext,useEffect, useState, useCallback} from 'react'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const AppContext = React.createContext();
+
+const AppProvider = ({children}) => {
+    const [loading, setLoading] = useState(true);
+    const [searchTerm, setSearchTerm] = useState('b');
+    const [product, setProduct] = useState([]);
+
+    const fetchProduct = useCallback( async () => {
+        setLoading(true);
+        try {
+            const response = await fetch (`${url}${searchTerm}`)
+            const data = await response.json();
+            const {drinks} = data
+            console.log(drinks);
+            if (drinks) {
+                const newProduct = drinks.map((item) => {
+                    const {
+                      idDrink,
+                      strDrink,
+                      strDrinkThumb,
+                      strAlcoholic,
+                      strGlass,
+                      strCategory,
+                      strInstructions,
+                      strInstructionsDE,
+                      strInstructionsIT
+                    } = item
+          
+                    return {
+                      id: idDrink,
+                      name: strDrink,
+                      image: strDrinkThumb,
+                      info: strAlcoholic,
+                      glass: strGlass,
+                      category: strCategory,
+                      IntroOne:strInstructions,
+                      Introtwo: strInstructionsDE,
+                      IntroThree: strInstructionsIT
+                    }
+                  })
+                setProduct(newProduct);
+            } else {
+                setProduct([])
+            }
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false)
+        }
+    },[searchTerm])
+
+    useEffect(() =>{
+        fetchProduct();
+    },[searchTerm,fetchProduct])
+
+    return <AppContext.Provider value={{loading,product,searchTerm,setSearchTerm}}>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
+
+export {AppContext,AppProvider}
